ecd91aea6832cfc4eb4814140d4afa9ea321c145
PedrixzZDev <pedrixzz6@gmail.com>
2024-10-13 15:13:14 -0300

    Update PlayerListHudMixin.java

--- src/main/java/promej/playerlistheads/ru/mixin/PlayerListHudMixin.java
+++ src/main/java/promej/playerlistheads/ru/mixin/PlayerListHudMixin.java
@@ -1,20 +1,22 @@
 package promej.playerlistheads.ru.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.PlayerListHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Environment(EnvType.CLIENT)
 @Mixin(PlayerListHud.class)
 public abstract class PlayerListHudMixin {
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;isLocalPlayer()Z"))
     private boolean renderHeads(MinecraftClient instance) {
-        return true;
+        // Certifique-se de que este código só é executado no cliente
+        if (instance.isInSingleplayer()) {
+            return true;
+        }
+        return false; // Lógica padrão no servidor
     }
-
 }
